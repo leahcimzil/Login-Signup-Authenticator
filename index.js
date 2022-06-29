417daaa43b0fe1865eb2eeb0542965bc6df7c1ed
leahcimzii <michaeloezil@gmail.com>
2022-06-29 05:52:41 +0100

    create ejs templates

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 require('./db')();
 
+
+app.use(express.json());
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=> res.render('home'));
+
+
 const port = process.env.port || 3000;
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
